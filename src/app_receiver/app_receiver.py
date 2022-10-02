89a71d78be7f3424e91bf8b1428c2ae578af2717
Mark Andreev <mark.andreev@gmail.com>
2022-10-02 21:21:56 +0100

    Add dependency injection to app receiver

--- src/app_receiver/app_receiver.py
+++ src/app_receiver/app_receiver.py
@@ -30,42 +30,42 @@ class TaskCreator:
         }))
 
 
-task_creator = TaskCreator()
+class BotCommandDispatcher:
+    def __init__(self, task_creator: TaskCreator):
+        self._task_creator = task_creator
 
-
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
-
-
-def echo(update: Update, context: CallbackContext) -> None:
-    if len(update.message.photo) > 0:
-        task_creator.create(
-            context.bot.get_file(update.message.photo[-1].file_id).file_path,
-            update.message.chat_id
+    def start(self, update: Update, context: CallbackContext) -> None:
+        user = update.effective_user
+        update.message.reply_markdown_v2(
+            fr'Hi {user.mention_markdown_v2()}\!',
+            reply_markup=ForceReply(selective=True),
         )
-        return
-    if (update.message.effective_attachment is not None
-            and update.message.effective_attachment.thumb is not None):
-        task_creator.create(
-            context.bot.get_file(update.message.effective_attachment.thumb.file_id).file_path,
-            update.message.chat_id
-        )
+
+    def help_command(self, update: Update, context: CallbackContext) -> None:
+        update.message.reply_text('Help!')
+
+    def echo(self, update: Update, context: CallbackContext) -> None:
+        if len(update.message.photo) > 0:
+            self._task_creator.create(
+                context.bot.get_file(update.message.photo[-1].file_id).file_path,
+                update.message.chat_id
+            )
+            return
+        if (update.message.effective_attachment is not None
+                and update.message.effective_attachment.thumb is not None):
+            self._task_creator.create(
+                context.bot.get_file(update.message.effective_attachment.thumb.file_id).file_path,
+                update.message.chat_id
+            )
 
 
 def main() -> None:
+    bot_command_dispatcher = BotCommandDispatcher(TaskCreator())
     updater = Updater(os.environ["TELEGRAM_BOT_TOKEN"])
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(~Filters.command, echo))
+    dispatcher.add_handler(CommandHandler("start", bot_command_dispatcher.start))
+    dispatcher.add_handler(CommandHandler("help", bot_command_dispatcher.help_command))
+    dispatcher.add_handler(MessageHandler(~Filters.command, bot_command_dispatcher.echo))
     updater.start_polling()
     updater.idle()
 
