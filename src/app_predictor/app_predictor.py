1d3031ebfb3c07ac18ac27f1209d0a5648c8f989
Mark Andreev <mark.andreev@gmail.com>
2022-10-10 22:25:25 +0100

    Add persistent storage
    - Implement save predicts to storage
    - Implement save source images to storage
    - Add s3 storage
    - Add reaction placeholder

--- src/app_predictor/app_predictor.py
+++ src/app_predictor/app_predictor.py
@@ -1,15 +1,22 @@
+import abc
+import datetime
 import json
 import logging
 import os
 import tempfile
 import time
 import typing
+import uuid
 import warnings
+from json import JSONEncoder
 
+import boto3
 import cv2
 import redis
 import requests
 from telegram import Bot
+from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
+from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -17,6 +24,46 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 CACHE = {}
+
+
+class PredictWriter(abc.ABC):
+    @abc.abstractmethod
+    def save(self, req, predicts):
+        pass
+
+
+class S3PredictWriter(PredictWriter):
+    @classmethod
+    def init(cls):
+        return S3PredictWriter(
+            endpoint_url=os.environ["S3_ENDPOINT_URL"],
+            access_key_id=os.environ["S3_ACCESS_KEY_ID"],
+            secret_access_key=os.environ["S3_SECRET_ACCESS_KEY"],
+            bucket_name=os.environ["S3_BUCKET_NAME"],
+            path_prefix=os.environ["S3_PATH_PREFIX"],
+            verify=os.environ.get("S3_VERIFY", "false") == "true"
+        )
+
+    def __init__(self, endpoint_url, access_key_id, secret_access_key, bucket_name, path_prefix, verify=False):
+        self._s3 = boto3.resource(
+            's3',
+            endpoint_url=endpoint_url,  # 'https://<minio>:9000'
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=None,
+            config=boto3.session.Config(signature_version='s3v4'),
+            verify=verify
+        )
+        self._bucket_name = bucket_name
+        self._path_prefix = path_prefix
+
+    def save(self, req, predicts):
+        obj_name = f"{self._path_prefix}/{datetime.datetime.now().timestamp()}-{uuid.uuid4()}.json"
+        obj = self._s3.Object(self._bucket_name, obj_name)
+        obj.put(Body=json.dumps({
+            "req": req,
+            "predicts": predicts
+        }, cls=CustomEncoder))
 
 
 class Rect:
@@ -30,6 +77,19 @@ class Rect:
 
     def __repr__(self):
         return f"Rect({self.x}, {self.y}, {self.w}, {self.h})"
+
+    def __dict__(self):
+        return {
+            "x": int(self.x),
+            "y": int(self.y),
+            "w": int(self.w),
+            "h": int(self.h),
+        }
+
+
+class CustomEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__()
 
 
 def predict(img) -> typing.List[Rect]:
@@ -62,13 +122,15 @@ def blur(img, rects: typing.List[Rect]):
     return img
 
 
-def transform(src_filename, desc_filename):
+def transform(predict_saver, req, src_filename, desc_filename):
     img = cv2.imread(src_filename)
     faces = predict(img)
+
     logger.info(f"Found '{len(faces)}' faces")
     if faces:
         logger.info(f"Rects: '{faces}'")
         img = blur(img, faces)
+    predict_saver.save(req, faces)
 
     cv2.imwrite(desc_filename, img)
 
@@ -76,27 +138,95 @@ def transform(src_filename, desc_filename):
 class DataReceiver:
     def __init__(self):
         self._r = redis.Redis(host=os.environ["REDIS_HOST"], port=os.environ["REDIS_PORT"], db=0)
+        self._q = os.environ["REDIS_QUEUE"]
 
     def __iter__(self):
         return self
 
     def __next__(self):
         while True:
-            req: bytes = self._r.lpop(os.environ["REDIS_QUEUE"])
+            req: bytes = self._r.lpop(self._q)
             if req is None:
                 time.sleep(1)
                 continue
             return json.loads(req.decode())
 
 
-def load_image(req, dest_filename):
-    logger.info(f"Load {req}")
-    resp = requests.get(req["img"])
-    if not resp.ok:
-        raise ValueError(resp)
+class BlobStorageReader(abc.ABC):
+    @abc.abstractmethod
+    def storage(self):
+        pass
 
-    with open(dest_filename, 'wb') as f:
-        f.write(resp.content)
+    @abc.abstractmethod
+    def load(self, file_link, dest_filename):
+        pass
+
+
+class RemoteLinkBlobStorageReader(BlobStorageReader):
+    @classmethod
+    def init(cls, storages_):
+        s = RemoteLinkBlobStorageReader()
+        storages_[s.storage] = s
+
+    @property
+    def storage(self):
+        return "REMOTE_LINK"
+
+    def load(self, file_link, dest_filename):
+        resp = requests.get(file_link)
+        if not resp.ok:
+            raise ValueError(resp)
+
+        with open(dest_filename, 'wb') as f:
+            f.write(resp.content)
+
+
+class S3BlobStorageReader(BlobStorageReader):
+    @classmethod
+    def init(cls, storages_):
+        try:
+            s = S3BlobStorageReader(
+                endpoint_url=os.environ["S3_ENDPOINT_URL"],
+                access_key_id=os.environ["S3_ACCESS_KEY_ID"],
+                secret_access_key=os.environ["S3_SECRET_ACCESS_KEY"],
+                bucket_name=os.environ["S3_BUCKET_NAME"],
+                verify=os.environ.get("S3_VERIFY", "false") == "true"
+            )
+            storages_[s.storage] = s
+        except Exception as e:
+            logger.info(f"Skip S3BlobStorageLoader creation.")
+            logger.info(e)
+
+    def __init__(self, endpoint_url, access_key_id, secret_access_key, bucket_name, verify=False):
+        self._s3 = boto3.resource(
+            's3',
+            endpoint_url=endpoint_url,  # 'https://<minio>:9000'
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=None,
+            config=boto3.session.Config(signature_version='s3v4'),
+            verify=verify
+        )
+        self._bucket_name = bucket_name
+
+    @property
+    def storage(self):
+        return "S3"
+
+    def load(self, file_link, dest_filename):
+        bucket, obj_name = file_link["bucket"], file_link["obj_name"]
+        with open(dest_filename, 'wb') as f:
+            f.write(self._s3.Object(bucket, obj_name).get()['Body'].read())
+
+
+def load_image(req, dest_filename, storages: typing.Dict[str, BlobStorageReader]):
+    logger.info(f"Load {req}")
+    storage, href = req["img"]["storage"], req["img"]["href"]
+
+    if storage not in storages:
+        raise ValueError(f"Unknown storage = {storage}")
+
+    storages[storage].load(href, dest_filename)
 
 
 class DataUploader:
@@ -105,19 +235,42 @@ class DataUploader:
 
     def upload(self, req, out_file):
         with open(out_file, "rb") as f:
-            self._b.send_photo(chat_id=req["chat_id"], photo=f)
+            # reply_to_message_id
+            self._b.send_photo(
+                chat_id=req["chat_id"],
+                photo=f,
+                reply_to_message_id=req["message_id"],
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("👍", callback_data='like'),
+                            InlineKeyboardButton("👎", callback_data='dislike'),
+                        ]
+                    ]
+                )
+            )
+
+
+def init_storages():
+    storages = {}
+    for s in [RemoteLinkBlobStorageReader, S3BlobStorageReader]:
+        s.init(storages)
+    return storages
 
 
 def main():
     data_uploader = DataUploader()
+    storages = init_storages()
+    predict_saver = S3PredictWriter.init()
+
     for req in DataReceiver():
         try:
             with tempfile.TemporaryDirectory() as tdir:
                 in_file = os.path.join(tdir, "input.jpg")
                 out_file = os.path.join(tdir, "output.jpg")
 
-                load_image(req, in_file)
-                transform(in_file, out_file)
+                load_image(req, in_file, storages)
+                transform(predict_saver, req, in_file, out_file)
                 data_uploader.upload(req, out_file)
         except Exception as e:
             logging.error(e)
--- src/app_receiver/app_receiver.py
+++ src/app_receiver/app_receiver.py
@@ -1,11 +1,16 @@
+import abc
 import json
 import logging
 import os
+import uuid
 
+import boto3
 import redis
+import requests
 from telegram import ForceReply
 from telegram import Update
 from telegram.ext import CallbackContext
+from telegram.ext import CallbackQueryHandler
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
@@ -19,20 +24,90 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+class BlobStorageWriter(abc.ABC):
+    @abc.abstractmethod
+    def save(self, file_link):
+        pass
+
+
+class RemoteLinkBlobStorageWriter(BlobStorageWriter):
+    def save(self, file_link):
+        return {
+            "storage": "REMOTE_LINK",
+            "href": file_link,
+        }
+
+
+class S3BlobStorageWriter(BlobStorageWriter):
+    def __init__(self, endpoint_url, access_key_id, secret_access_key, bucket_name, path_prefix, verify=False):
+        self._s3 = boto3.resource(
+            's3',
+            endpoint_url=endpoint_url,  # 'https://<minio>:9000'
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=None,
+            config=boto3.session.Config(signature_version='s3v4'),
+            verify=verify
+        )
+        self._bucket_name = bucket_name
+        self._path_prefix = path_prefix
+
+    def save(self, file_link):
+        obj_name = f"{self._path_prefix}/{uuid.uuid4()}.jpg"
+        obj = self._s3.Object(self._bucket_name, obj_name)
+        obj.put(Body=self._load_content(file_link))
+        return {
+            "storage": "S3",
+            "href": {
+                "bucket": self._bucket_name,
+                "obj_name": obj_name
+            },
+        }
+
+    @classmethod
+    def _load_content(cls, file_link):
+        resp = requests.get(file_link)
+        if not resp.ok:
+            raise ValueError(resp)
+        return resp.content
+
+
+def new_blob_storage():
+    provider = os.environ.get("BLOB_STORAGE_PROVIDER", "REMOTE_LINK")
+    if provider == "REMOTE_LINK":
+        return RemoteLinkBlobStorageWriter()
+    elif provider == "S3":
+        return S3BlobStorageWriter(
+            endpoint_url=os.environ["S3_ENDPOINT_URL"],
+            access_key_id=os.environ["S3_ACCESS_KEY_ID"],
+            secret_access_key=os.environ["S3_SECRET_ACCESS_KEY"],
+            bucket_name=os.environ["S3_BUCKET_NAME"],
+            path_prefix=os.environ["S3_PATH_PREFIX"],
+            verify=os.environ.get("S3_VERIFY", "false") == "true"
+        )
+    else:
+        raise ValueError(f"Unknown blobStorageProvider = '{provider}'")
+
+
 class TaskCreator:
     def __init__(self):
         self._r = redis.Redis(host=os.environ["REDIS_HOST"], port=os.environ["REDIS_PORT"], db=0)
+        self._q = os.environ["REDIS_QUEUE"]
 
-    def create(self, img_url, chart_id):
-        self._r.rpush(os.environ["REDIS_QUEUE"], json.dumps({
-            "img": img_url,
-            "chat_id": chart_id
+    def create(self, img_data, chart_id, message_id):
+        self._r.rpush(self._q, json.dumps({
+            "img": img_data,
+            "chat_id": chart_id,
+            "message_id": message_id,
         }))
 
 
 class BotCommandDispatcher:
-    def __init__(self, task_creator: TaskCreator):
+    def __init__(self,
+                 task_creator: TaskCreator,
+                 blob_storage: BlobStorageWriter):
         self._task_creator = task_creator
+        self._blob_storage = blob_storage
 
     def start(self, update: Update, context: CallbackContext) -> None:
         user = update.effective_user
@@ -44,28 +119,34 @@ class BotCommandDispatcher:
     def help_command(self, update: Update, context: CallbackContext) -> None:
         update.message.reply_text('Upload image as picture. Bot will blur all detected faces.')
 
+    def receive_callback(self, update: Update, context: CallbackContext) -> None:
+        pass
+
     def echo(self, update: Update, context: CallbackContext) -> None:
         if len(update.message.photo) > 0:
             self._task_creator.create(
-                context.bot.get_file(update.message.photo[-1].file_id).file_path,
-                update.message.chat_id
+                self._blob_storage.save(context.bot.get_file(update.message.photo[-1].file_id).file_path),
+                update.message.chat_id,
+                update.message.message_id,
             )
             return
         if (update.message.effective_attachment is not None
                 and update.message.effective_attachment.thumb is not None):
             self._task_creator.create(
                 context.bot.get_file(update.message.effective_attachment.thumb.file_id).file_path,
-                update.message.chat_id
+                update.message.chat_id,
+                update.message.message_id,
             )
 
 
 def main() -> None:
-    bot_command_dispatcher = BotCommandDispatcher(TaskCreator())
+    bot_command_dispatcher = BotCommandDispatcher(TaskCreator(), new_blob_storage())
     updater = Updater(os.environ["TELEGRAM_BOT_TOKEN"])
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", bot_command_dispatcher.start))
     dispatcher.add_handler(CommandHandler("help", bot_command_dispatcher.help_command))
     dispatcher.add_handler(MessageHandler(~Filters.command, bot_command_dispatcher.echo))
+    dispatcher.add_handler(CallbackQueryHandler(bot_command_dispatcher.receive_callback))
     updater.start_polling()
     updater.idle()
 
