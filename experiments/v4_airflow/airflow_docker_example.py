f2ba9324cd6b04261813ffa22647853cf2c20e9f
Mark Andreev <mark.andreev@gmail.com>
2022-10-26 16:41:43 +0100

    Fix airflow dag code style

--- experiments/v4_airflow/airflow_docker_example.py
+++ experiments/v4_airflow/airflow_docker_example.py
@@ -10,24 +10,20 @@ with DAG(
         catchup=False,
         tags=["example", "docker"],
 ) as dag:
-    t1 = BashOperator(task_id='print_date', bash_command='date', dag=dag)
-    t2 = BashOperator(task_id='sleep', bash_command='sleep 5', retries=3, dag=dag)
+    t1 = BashOperator(
+        task_id="print_date",
+        bash_command="date",
+        dag=dag
+    )
+    t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
     t3 = DockerOperator(
-        docker_url='unix://var/run/docker.sock',  # Set your docker URL
-        command='/bin/sleep 30',
-        image='centos:latest',
-        network_mode='bridge',
-        task_id='docker_op_tester',
+        docker_url="unix://var/run/docker.sock",  # Set your docker URL
+        command="/bin/sleep 30",
+        image="centos:latest",
+        network_mode="bridge",
+        task_id="docker_op_tester",
         dag=dag,
     )
-    t4 = BashOperator(task_id='print_hello', bash_command='echo "hello world!!!"', dag=dag)
-    # t1 >> t2
-    # t1 >> t3
-    # t3 >> t4
+    t4 = BashOperator(task_id="print_hello", bash_command="echo \"hello world!!!\"", dag=dag)
 
-    (
-        # TEST BODY
-            t1
-            >> [t2, t3]
-            >> t4
-    )
+    t1 >> [t2, t3] >> t4
