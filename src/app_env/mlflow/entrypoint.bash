061d73f76ba6bbb7d8723f653fc30762f12051c6
Mark Andreev <mark.andreev@gmail.com>
2022-11-07 18:06:46 +0000

    Add docker registry

--- src/app_env/mlflow/entrypoint.bash
+++ src/app_env/mlflow/entrypoint.bash
@@ -1,4 +1,5 @@
 mlflow ui \
   --host 0.0.0.0 \
+  --port 5500 \
   --backend-store-uri postgresql://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME \
   --serve-artifacts --artifacts-destination s3://$S3_BUCKET/$S3_BUCKET_PREFIX
