d4101273dbf31c70b0056b91b8a37f1a101c0d40
Mark Andreev <mark.andreev@gmail.com>
2022-11-07 18:03:07 +0000

    Add standalone airflow

--- src/app_env/mlflow/entrypoint.bash
+++ src/app_env/mlflow/entrypoint.bash
@@ -1,3 +1,4 @@
 mlflow ui \
+  --host 0.0.0.0 \
   --backend-store-uri postgresql://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME \
   --serve-artifacts --artifacts-destination s3://$S3_BUCKET/$S3_BUCKET_PREFIX
