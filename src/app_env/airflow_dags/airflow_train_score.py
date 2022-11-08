e8a0dd9a4fb1188dfdbf3432f082198f3a3eb505
Mark Andreev <mark.andreev@gmail.com>
2022-11-08 17:00:42 +0000

    Extract secrets to envs

--- src/app_env/airflow_dags/airflow_train_score.py
+++ src/app_env/airflow_dags/airflow_train_score.py
@@ -1,9 +1,16 @@
+import os
 from datetime import datetime
 
 from airflow.models import DAG
 from airflow.operators.python import task
 from airflow.providers.docker.operators.docker import DockerOperator
 from mlflow import MlflowClient
+
+MODEL_IMAGE = os.environ['ENDPOINT_IMAGE_REGISTRY'] + 'model:latest'
+INNER_TRACKING_URI = os.environ['ENDPOINT_MLFLOW_TRACKING_URL_INNER_URL']
+MODEL_NAME = 'yolov5'
+S3_BUCKET_DATASET_PREFIX = 'dataset'
+DEVICE = 'cpu'
 
 with DAG(
         'yolov5_train_score',
@@ -15,50 +22,47 @@ with DAG(
     train = DockerOperator(
         docker_url='unix://var/run/docker.sock',
         command='train.py',
-        image='localhost:5000/model:latest',
+        image=MODEL_IMAGE,
         network_mode='host',
         task_id='train',
         dag=dag,
         shm_size=2147483648,
         environment={
-            'S3_ENDPOINT': 'http://localhost:9000',
-            'S3_USER': 'miniouser',
-            'S3_PASSWORD': 'miniopassword',
-            'S3_BUCKET': 'mlsystemdesign22',
-            'S3_BUCKET_DATASET_PREFIX': 'dataset',
-            'DEVICE': 'cpu',
-            'MLFLOW_TRACKING_URL': 'http://localhost:5500',
+            'S3_ENDPOINT': os.environ['SECRET_S3_ENDPOINT'],
+            'S3_USER': os.environ['SECRET_S3_ACCESS_TOKEN'],
+            'S3_PASSWORD': os.environ['SECRET_S3_SECRET_TOKEN'],
+            'S3_BUCKET': os.environ['SECRET_S3_BUCKET'],
+            'S3_BUCKET_DATASET_PREFIX': S3_BUCKET_DATASET_PREFIX,
+            'DEVICE': DEVICE,
+            'MLFLOW_TRACKING_URL': os.environ['ENDPOINT_MLFLOW_TRACKING_URL_EXTERNAL_URL'],
         }
     )
 
     score = DockerOperator(
         docker_url='unix://var/run/docker.sock',
         command='score.py',
-        image='localhost:5000/model:latest',
+        image=MODEL_IMAGE,
         network_mode='host',
         task_id='score',
         dag=dag,
         shm_size=2147483648,
         environment={
-            'S3_ENDPOINT': 'http://localhost:9000',
-            'S3_USER': 'miniouser',
-            'S3_PASSWORD': 'miniopassword',
-            'S3_BUCKET': 'mlsystemdesign22',
-            'S3_BUCKET_DATASET_PREFIX': 'dataset',
-            'DEVICE': 'cpu',
-            'MLFLOW_TRACKING_URL': 'http://localhost:5500',
-            'MODEL_NAME': 'yolov5',
+            'S3_ENDPOINT': os.environ['SECRET_S3_ENDPOINT'],
+            'S3_USER': os.environ['SECRET_S3_ACCESS_TOKEN'],
+            'S3_PASSWORD': os.environ['SECRET_S3_SECRET_TOKEN'],
+            'S3_BUCKET': os.environ['SECRET_S3_BUCKET'],
+            'S3_BUCKET_DATASET_PREFIX': S3_BUCKET_DATASET_PREFIX,
+            'DEVICE': DEVICE,
+            'MLFLOW_TRACKING_URL': os.environ['ENDPOINT_MLFLOW_TRACKING_URL_EXTERNAL_URL'],
+            'MODEL_NAME': MODEL_NAME,
             'MODEL_STAGE': 'Staging'
         }
     )
 
 
     @task(task_id="transition_model_version_stage_to_staging")
-    def transition_model_version_stage_to_staging():
-        TRACKING_URI = 'http://mlflow:5500'
-        MODEL_NAME = 'yolov5'
-
-        client = MlflowClient(TRACKING_URI)
+    def transition_model_version_stage_to_staging(tracking_url):
+        client = MlflowClient(tracking_url)
         model = client.get_registered_model(MODEL_NAME)
         last_version = model.latest_versions[-1]
         client.transition_model_version_stage(
@@ -69,11 +73,8 @@ with DAG(
 
 
     @task(task_id="transition_model_version_stage_to_production")
-    def transition_model_version_stage_to_production():
-        TRACKING_URI = 'http://mlflow:5500'
-        MODEL_NAME = 'yolov5'
-
-        client = MlflowClient(TRACKING_URI)
+    def transition_model_version_stage_to_production(tracking_url):
+        client = MlflowClient(tracking_url)
         model = client.get_registered_model(MODEL_NAME)
         last_version = model.latest_versions[-1]
         client.transition_model_version_stage(
@@ -83,4 +84,9 @@ with DAG(
         )
 
 
-    train >> transition_model_version_stage_to_staging() >> score >> transition_model_version_stage_to_production()
+    (
+            train >>
+            transition_model_version_stage_to_staging(INNER_TRACKING_URI) >>
+            score >>
+            transition_model_version_stage_to_production(INNER_TRACKING_URI)
+    )
