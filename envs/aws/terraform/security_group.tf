850ff7e805b48f67b8897505c98517c335ca8422
Mark Andreev <mark.andreev@gmail.com>
2022-12-02 17:29:45 +0000

    Add terraform for aws env

---
+++ envs/aws/terraform/security_group.tf
@@ -0,0 +1,27 @@
+
+resource "aws_security_group" "this" {
+  name = "sg_ml_system_design_22"
+
+  tags = {
+    Project = "ml-system-design-22"
+  }
+}
+
+resource "aws_security_group_rule" "ingress_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.this.id
+}
---
+++ envs/aws/terraform/settings.tf
@@ -0,0 +1,8 @@
+variable "ami" {
+  default = "ami-0574da719dca65348"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
