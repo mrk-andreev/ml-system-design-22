e8205b18de58834698f4cb71b6780bb11b55ffae
Mark Andreev <mark.andreev@gmail.com>
2022-12-09 11:33:23 +0000

    Add aws_s3_bucket_public_access_block

--- envs/aws/terraform/bucket.tf
+++ envs/aws/terraform/bucket.tf
@@ -10,3 +10,12 @@ resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
