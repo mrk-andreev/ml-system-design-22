12781ed6dec355adb235db125567a8718906f2eb
Mark Andreev <mark.andreev@gmail.com>
2022-12-10 14:28:51 +0000

    Add docker-compose copy to bucket

--- envs/aws/terraform/bucket.tf
+++ envs/aws/terraform/bucket.tf
@@ -19,3 +19,9 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_object" "docker-compose" {
+  bucket         = var.bucket_name
+  key            = "docker-compose.yaml"
+  content_base64 = base64encode(file("docker-compose.yaml"))
+}
