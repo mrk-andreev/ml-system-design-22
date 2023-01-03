5b0028c2f316063c7696a386142eeb3b18b824c9
Mark Andreev <mark.andreev@gmail.com>
2023-01-03 23:00:47 +0000

    Add profilers to dev deps

--- src/app_predictor/test_predictor.py
+++ src/app_predictor/test_predictor.py
@@ -1,12 +1,13 @@
 import os
+import time
 
 import cv2
 import pytest
 
-from src.app_predictor.predict import BiSeNetPredictor
-from src.app_predictor.app_predictor import blur
 from src.app_predictor.app_predictor import Cv2CascadeClassifierPredictor
 from src.app_predictor.app_predictor import Rect
+from src.app_predictor.app_predictor import blur
+from src.app_predictor.predict import BiSeNetPredictor
 
 IMAGE_IN = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data/sample-0.png")
 IMAGE_OUT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data/sample-0_out.png")
@@ -36,6 +37,38 @@ def test_bisenet_predict(bisenet_predictor):
     img = cv2.imread(IMAGE_IN)
     _, faces = bisenet_predictor.predict_from_picture(img)
     assert len(faces) >= 9
+
+
+def test_memory_besinet():
+    from memory_profiler import profile
+
+    @profile
+    def call():
+        predictor = BiSeNetPredictor()
+        predictor.load_context(None)
+        img = cv2.imread(IMAGE_IN)
+        predictor.predict_from_picture(img)
+
+    call()
+
+
+def test_speed_besinet():
+    predictor = BiSeNetPredictor()
+    predictor.load_context(None)
+    img = cv2.imread(IMAGE_IN)
+
+    def call():
+        predictor.predict_from_picture(img)
+
+    def calc():
+        t = time.time()
+        call()
+        return time.time() - t
+
+    print([
+        calc()
+        for _ in range(10)
+    ])
 
 
 def test_blur():
